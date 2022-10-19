d1fafd1e60b089ccff66257227197a8a48c32fc2
Sovathana Phat <sophatvathana@Sovathanas-MBP.lan>
2022-10-19 11:24:24 +0700

    fixed error path in log stream

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,42 +3,40 @@ import { User } from "../interface/user.interface";
 import AuthService from "../services/auth.service";
 import UserService from "../services/user.service";
 
-export default class AuthController{
-    public userService = new UserService;
-    public authService = new AuthService;
-    public signUp = async (req:Request,res:Response, next:NextFunction)=>{
-        try{
-            await this.userService.createUser({
-                email: req.body.email,
-                username: req.body.username,
-                gender: req.body.gender,
-            })
-            res.status(201).json({"Response":"User Created Successfully"})
-        }catch(error){
-            next(error)
-        }
+export default class AuthController {
+  public userService = new UserService();
+  public authService = new AuthService();
+  public signUp = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await this.userService.createUser({
+        email: req.body.email,
+        username: req.body.username,
+        gender: req.body.gender,
+      });
+      res.status(201).json({ Response: "User Created Successfully" });
+    } catch (error) {
+      next(error);
     }
-    public logIn = async (req:Request, res:Response, next:NextFunction)=>{
-        try{
-            const {userInfo , cookie} = await this.authService.logIn(
-                req.body.email,
-                req.body.password
-            )
-            res.setHeader('Set-Cookie', [cookie]);
-            res.status(200).json({ data: userInfo, message: 'login' });
-        } catch (error) {
-            next(error);
-        }
+  };
+  public logIn = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { userInfo, cookie } = await this.authService.logIn(
+        req.body.email,
+        req.body.password
+      );
+      // res.setHeader('Set-Cookie', [cookie]);
+      res.status(200).json({ data: userInfo, message: "login" });
+    } catch (error) {
+      next(error);
     }
+  };
 
-    public logOut = async (req:Request, res:Response, next:NextFunction) => {
-        try {
-            const logOutUserData = await this.authService.logOut();
-      
-            res.setHeader('Set-Cookie', ['Authorization=; Max-age=0']);
-            res.status(200).json({ data: logOutUserData, message: 'logout' });
-        } catch (error) {
-            
-        }
-    }
+  public logOut = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const logOutUserData = await this.authService.logOut();
+
+      // res.setHeader('Set-Cookie', ['Authorization=; Max-age=0']);
+      res.status(200).json({ data: logOutUserData, message: "logout" });
+    } catch (error) {}
+  };
 }
--- src/utils/logger.util.ts
+++ src/utils/logger.util.ts
@@ -1,17 +1,19 @@
-import { existsSync, mkdirSync } from 'fs';
-import { join } from 'path';
-import winston from 'winston';
-import winstonDaily from 'winston-daily-rotate-file';
+import { existsSync, mkdirSync } from "fs";
+import { join } from "path";
+import winston from "winston";
+import winstonDaily from "winston-daily-rotate-file";
 
 // logs dir
-const logDir: string = join(__dirname, process.env.LOG_DIR);
+const logDir: string = join(__dirname, process.env.LOG_DIR ?? "../../logs");
 
 if (!existsSync(logDir)) {
   mkdirSync(logDir);
 }
 
 // Define log format
-const logFormat = winston.format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`);
+const logFormat = winston.format.printf(
+  ({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`
+);
 
 /*
  * Log Level
@@ -20,16 +22,16 @@ const logFormat = winston.format.printf(({ timestamp, level, message }) => `${ti
 const logger = winston.createLogger({
   format: winston.format.combine(
     winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
+      format: "YYYY-MM-DD HH:mm:ss",
     }),
-    logFormat,
+    logFormat
   ),
   transports: [
     // debug log setting
     new winstonDaily({
-      level: 'debug',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/debug', // log file /logs/debug/*.log in save
+      level: "debug",
+      datePattern: "YYYY-MM-DD",
+      dirname: logDir + "/debug", // log file /logs/debug/*.log in save
       filename: `%DATE%.log`,
       maxFiles: 30, // 30 Days saved
       json: false,
@@ -37,9 +39,9 @@ const logger = winston.createLogger({
     }),
     // error log setting
     new winstonDaily({
-      level: 'error',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/error', // log file /logs/error/*.log in save
+      level: "error",
+      datePattern: "YYYY-MM-DD",
+      dirname: logDir + "/error", // log file /logs/error/*.log in save
       filename: `%DATE%.log`,
       maxFiles: 30, // 30 Days saved
       handleExceptions: true,
@@ -51,13 +53,16 @@ const logger = winston.createLogger({
 
 logger.add(
   new winston.transports.Console({
-    format: winston.format.combine(winston.format.splat(), winston.format.colorize()),
-  }),
+    format: winston.format.combine(
+      winston.format.splat(),
+      winston.format.colorize()
+    ),
+  })
 );
 
 const stream = {
   write: (message: string) => {
-    logger.info(message.substring(0, message.lastIndexOf('\n')));
+    logger.info(message.substring(0, message.lastIndexOf("\n")));
   },
 };
 
